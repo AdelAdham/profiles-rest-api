949e9e495f3675fd9d878bc1b94a8f71a7f2ac67
Adel <adeladham95@gmail.com>
2025-03-11 22:13:40 +0100

    adding prfole viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 
 from profiles_api import serializers
+from profiles_api import models
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -92,3 +93,12 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
          """Handle removing an object"""
          return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
+    # once creating model serializer Django already knows the actions
+    # ex: list, craete, update....
