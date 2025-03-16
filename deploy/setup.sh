4faf8941c14e95fab2b3b74c5900d18d26a80698
Adel <adeladham95@gmail.com>
2025-03-17 00:03:44 +0100

    added allowed hosts

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -10,17 +10,16 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
 
-# Install Python, SQLite and pip
-# echo "Installing dependencies..."
-# apt-get update
-# apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
-#
-# mkdir -p $PROJECT_BASE_PATH
-# git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
-# 
-# python3 -m venv $PROJECT_BASE_PATH/env
+echo "Installing dependencies..."
+apt-get update
+apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+mkdir -p $PROJECT_BASE_PATH
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+
+python3 -m venv $PROJECT_BASE_PATH/env
+
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi
 
 # Run migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
